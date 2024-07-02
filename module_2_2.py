c16ac2a3cd4e09d4044c42b1e18d4b2ec9a0586c
Skip-GT <yaroslav.vasilev.2015@gmail.com>
2024-07-02 18:04:12 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,11 @@
+print('Введите три числа: ')
+a = int(input())
+b = int(input())
+c = int(input())
+if a == b and a == c and b == c:
+    print(3)
+elif a == b or a == c or b == c:
+    print(2)
+elif a != b and a != c and b != c:
+    print(0)
+
